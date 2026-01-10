a286dc91640d05532a7860be7fa88810e7402f11
capoduc <capoduc071@gmail.com>
2026-01-10 05:40:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "23673651"))
 API_HASH = os.environ.get("API_HASH", "f032bfa12ee46e1283f6fb23cfca5c6b")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7976841490:AAHRhdv4LOuMW87FE1EKkin26kN3LYQAmNU7976841490:AAHRhdv4LOuMW87FE1EKkin26kN3LYQAmNU")
 
 CREDIT = os.environ.get("CREDIT", "🇮🇳bantoo Singh 🇮🇳")
 # MongoDB Configuration
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
